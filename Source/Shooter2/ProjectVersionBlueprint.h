1096f24af57de35f5cefbaacea72ef3025d5a530
Justin Hanrahan <hanrahjust@myvuw.ac.nz>
2021-07-26 23:15:23 +1200

    Created C++ node for getting the project version. + version widget

---
+++ Source/Shooter2/ProjectVersionBlueprint.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ProjectVersionBlueprint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER2_API UProjectVersionBlueprint : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+		UFUNCTION(BlueprintPure, meta = (DisplayName = "Project Version", CompactNodeTitle = "ProjectVersion"), Category = "System Information")
+		static FString GetProjectVersion();
+
+
+};
